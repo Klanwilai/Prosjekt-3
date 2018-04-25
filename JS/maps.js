391443d135bb152f5771530cd28fdb06e2067dd1
Daniel Nielsen <daniel@Daniels-MBP.lan>
2018-04-25 23:12:34 +0200

    maps mostly done

--- JS/maps.js
+++ JS/maps.js
@@ -1,9 +1,16 @@
 const keyAPI = "AIzaSyB55RI2UNsn9pxfGOvIXT5FR-U1POKjdCk";
 var map;
-var mapElement;
+var mapElement = document.querySelector("#map");
+var mapInfo = document.querySelector("#map-info");
+var streetImg = document.querySelector("#img");
+var heading = [0, 90, 180, 270];    //degrees for heading when geetting street view images
 
+
+/**
+ *  Loads the google map, but only if the API is fully loaded
+ *  also adds functionality for when you click on the map
+ */
 window.addEventListener("load", e => {
-    mapElement = document.querySelector("#map");
     loadGoogleMapAPI().then(() => {
             console.log("map API loaded");
             map = new google.maps.Map(mapElement, {
@@ -11,17 +18,33 @@ window.addEventListener("load", e => {
                 zoom: 14,
             });
             map.addListener("click", e => {
-                showInfo({lat: e.latLng.lat(), len: e.latLng.len()});
+                showInfo({lat: e.latLng.lat(), lng: e.latLng.lng()});
             });
     });
 });
 
+
+/**
+ * Displays adress and streetview pictures on the webpage
+ * takes @param info as parameter, info being the coordinates of where you clicked on the map.
+ */
 function showInfo(info){
     console.log(info);
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${info.lat},${info.lng}&key=${keyAPI}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        mapInfo.innerHTML = data.results[0].formatted_address;
+    });
+    // loops 4 times, to insert streetview pictures into each of the img html tags. uses the var heading to get view for each direction.
+    for(i = 0; i <= 3; i++) {
+        document.querySelector(`#img${i}`).src = `https://maps.googleapis.com/maps/api/streetview?size=250x200&location=${info.lat},${info.lng}&fov=90&heading=${heading[i]}&key=${keyAPI}`;
+    }
 }
 
 /**
- *
+ * function that loads the API
+ * @return Promise which resolves if API is loaded.
  */
 function loadGoogleMapAPI() {
     return new Promise((resolve, reject) => {

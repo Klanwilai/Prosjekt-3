2aef7793da8aa31f58b4b0705d636c212969da98
Daniel Nielsen <daniel@public-142-155.hig.no>
2018-04-29 15:33:03 +0200

    added comments to user.js

--- JS/user.js
+++ JS/user.js
@@ -2,6 +2,12 @@ let html = "";
 var test;
 let users = [];
 
+
+/**
+ *  @class user contains name, email, adress and picture.
+ *  @constructor takes json results from @param fetchData and an @param i which is an int.
+ *  grabs data from fetchData with the index passed as i.
+ */
 class user {
     constructor(fetchData, i) {
         this.name = fetchData.results[i].name;
@@ -10,7 +16,11 @@ class user {
         this.picture = fetchData.results[i].picture.large;
     };
 
-
+    /**
+     *  class @function displayUser prints html to variable
+     *  @param dataToHTML is the variable html is written into
+     *  @return the passed variable after being updated with the html.
+     */
     displayUser(dataToHTML) {
         dataToHTML = `<div class="user-body"><img src="${this.picture}" class="user-image">
                 <ul class="user-info"><li>Name: ${this.name.first} ${this.name.last}</li>
@@ -21,7 +31,12 @@ class user {
 }
 
 
-
+/**
+ *  Runs when the page is loaded
+ *  fetches randomuser data, converts to json
+ *  loops through all 50 users, creating @class user objects
+ *  Calls class @function displayUser to print all user data to html
+ */
 window.addEventListener("load", e => {
     fetch("https://randomuser.me/api/?results=50")
     .then(res => res.json())

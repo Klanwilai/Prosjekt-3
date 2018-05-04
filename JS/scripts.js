b72c26ba54396a90c829260ef9d254f184a0232e
Daniel Nielsen <daniel@Daniels-MacBook-Pro.local>
2018-05-04 12:50:32 +0200

    comments

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,10 +1,19 @@
 var images = ["Images/lime01.jpg", "Images/lime02.jpg", "Images/lime03.jpg"];
 var currentImage = 0;
 
+
+/**
+ * When page is loaded, run @function loadImage
+ */
 window.addEventListener("load", e => {
     loadImage();
 });
 
+/**
+ * Creates new img element. When new element is loaded run function
+ * #slide1 source is set to be source of @var img
+ * @var img source is set to be first index of array iages
+ */
 function loadImage() {
     var img = document.createElement("img");
     img.addEventListener("load", e => {
@@ -16,7 +25,7 @@ function loadImage() {
 
 function slideshow() {
     currentImage++;
-    if(currentImage == images.length) {
+    if(currentImage === images.length) {
         currentImage = 0;
     }
     document.querySelector("#slide2").src = images[currentImage];
@@ -29,7 +38,7 @@ document.querySelector("#slide2").addEventListener("load", e => {
 document.querySelector("#slide2").addEventListener("transitioned", e => {
     document.querySelector("#slide1").src = document.querySelector("#slide2").src;
 
-    if(document.querySelector("#slide2").style.opacity = 1) {
+    if(document.querySelector("#slide2").style.opacity == 1) {
         document.querySelector("#slide2").style.opacity = 0;
         window.setTimeout(slideshow, 2000);
     }
